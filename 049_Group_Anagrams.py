3e67124059e5e2ae2793d0905b127941213c9904
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-07-11 18:02:50 +0530

    Add solution for LeetCode 049 Group Anagrams

---
+++ 049_Group_Anagrams.py
@@ -0,0 +1,19 @@
+def groupAnagrams(strs):
+
+    groups = {}
+
+    for word in strs:
+
+        key = "".join(sorted(word))
+
+        if key not in groups:
+            groups[key] = []
+
+        groups[key].append(word)
+
+    return list(groups.values())
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+print(groupAnagrams(strs))
