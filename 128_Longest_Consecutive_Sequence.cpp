8a18fc104ef1f39bce629d4de709c271442efcc6
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-07-15 20:40:21 +0530

    Add solution for LeetCode 128 Longest Consecutive Sequence in C++

---
+++ 128_Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+int longestConsecutive(vector<int>& nums)
+{
+    unordered_set<int> numSet(nums.begin(), nums.end());
+
+    int longest = 0;
+
+    for (int num : numSet)
+    {
+        if (numSet.find(num - 1) == numSet.end())
+        {
+            int current = num;
+            int streak = 1;
+
+            while (numSet.find(current + 1) != numSet.end())
+            {
+                current++;
+                streak++;
+            }
+
+            longest = max(longest, streak);
+        }
+    }
+
+    return longest;
+}
+
+int main()
+{
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+
+    cout << "Longest Consecutive Sequence Length: "
+         << longestConsecutive(nums) << endl;
+
+    return 0;
+}
