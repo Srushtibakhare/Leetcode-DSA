a1336414ce476e7c64deec61687f2f5be1a09e0e
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-07-15 20:46:18 +0530

    Add solution for LeetCode 125 Valid Palindrome in C

---
+++ 125_Valid_Palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+bool isPalindrome(char *s)
+{
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right)
+    {
+        while (left < right && !isalnum(s[left]))
+            left++;
+
+        while (left < right && !isalnum(s[right]))
+            right--;
+
+        if (tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main()
+{
+    char s[] = "A man, a plan, a canal: Panama";
+
+    if (isPalindrome(s))
+        printf("True\n");
+    else
+        printf("False\n");
+
+    return 0;
+}
