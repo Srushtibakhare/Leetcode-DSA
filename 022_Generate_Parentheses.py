ce61ffc08dfeacc8e9cbec02552d0d1ba193890c
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-07-19 11:29:31 +0530

    Add solution for LeetCode 22 Generate Parentheses in Python

---
+++ 022_Generate_Parentheses.py
@@ -0,0 +1,24 @@
+def generateParenthesis(n):
+
+    result = []
+
+    def backtrack(curr, openCount, closeCount):
+
+        if len(curr) == 2 * n:
+            result.append(curr)
+            return
+
+        if openCount < n:
+            backtrack(curr + "(", openCount + 1, closeCount)
+
+        if closeCount < openCount:
+            backtrack(curr + ")", openCount, closeCount + 1)
+
+    backtrack("", 0, 0)
+
+    return result
+
+
+n = 3
+
+print(generateParenthesis(n))
