f0b8c6e6b30c0b953727a3f68c23f16e2f457eb2
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-07-25 18:10:46 +0530

    Add solution for Leetcode 70 Climbing Stairs in Python

---
+++ 070_Climbing_Stairs.py
@@ -0,0 +1,19 @@
+def climbStairs(n):
+
+    if n == 1:
+        return 1
+
+    dp = [0] * (n + 1)
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+n = int(input("Enter number of stairs: "))
+
+print("Number of ways:", climbStairs(n))
