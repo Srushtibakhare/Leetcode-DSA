80568c5fa8ac2cb7cc15aa89aaf8aca89eeaafa2
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-07-26 12:49:56 +0530

    Add Solution for Leetcode 628 Maximum Product of three Numbers

---
+++ 628_Maximum_Product_of_3_Numbers.py
@@ -0,0 +1,14 @@
+def maximumProduct(nums):
+
+    nums.sort()
+    n = len(nums)
+
+    product1 = nums[n - 1] * nums[n - 2] * nums[n - 3]
+    product2 = nums[0] * nums[1] * nums[n - 1]
+
+    return max(product1, product2)
+
+
+nums = list(map(int, input("Enter numbers: ").split()))
+
+print("Maximum Product:", maximumProduct(nums))
